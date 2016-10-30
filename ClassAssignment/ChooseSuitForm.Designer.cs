e066541bc3a87abb6c9593ebb0a4b965b48fa563
Jonny Hall <jonnyhall@bigpond.com>
2016-10-30 17:03:46 +1000

    Crazy Eights Finished

--- ClassAssignment/ChooseSuitForm.Designer.cs
+++ ClassAssignment/ChooseSuitForm.Designer.cs
@@ -97,7 +97,7 @@
             this.button1.TabIndex = 4;
             this.button1.Text = "Ok";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // ChooseSuitForm
             // 
